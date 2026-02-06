d01f463bb904da92466c57d8ab8ef972ea062e02
Vajra <vajra172007@gmail.com>
2026-02-06 08:59:46 +0530

    Fix template rendering and file paths - Add UI templates and static files - Fix relative paths for cmd/web directory - Correct template syntax and structure - Enable proper HTML rendering

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -2,14 +2,33 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
 	"strconv"
+	"text/template"
 )
 
 func home(w http.ResponseWriter, r *http.Request){
 
 	w.Header().Add("Server", "Go")
-	w.Write([]byte("Hello from SnippetBox"))
+
+	files := []string{
+		"../../ui/html/base.html",
+		"../../ui/html/partials/nav.html",
+		"../../ui/html/pages/home.html",
+	}
+
+	ts, err := template.ParseFiles(files...)
+	if err != nil {
+		log.Print(err.Error())
+		http.Error(w,"Internal Server Error", http.StatusInternalServerError)
+		return
+	}
+	err = ts.ExecuteTemplate(w, "base", nil)
+	if err != nil{
+		log.Print(err.Error())
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+	}
 }
 func snippetView(w http.ResponseWriter, r *http.Request){
 	id , err := strconv.Atoi(r.PathValue("id"))
--- cmd/web/main.go
+++ cmd/web/main.go
@@ -7,10 +7,15 @@ import (
 
 func main(){
 	mux := http.NewServeMux()
+	fileServer := http.FileServer(http.Dir("../../ui/static/"))
+
+	mux.Handle("GET /static/", http.StripPrefix("/static",fileServer))
+
 	mux.HandleFunc("GET /{$}",home)
 	mux.HandleFunc("GET /snippet/view/{id}",snippetView)
 	mux.HandleFunc("GET /snippet/create", snippetCreate)
 	mux.HandleFunc("POST /snippet/create", snippetCreatePost)
+
 
 	err := http.ListenAndServe(":400",mux)
 	log.Fatal(err)
